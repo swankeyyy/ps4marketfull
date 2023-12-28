3b132fb68fdee6eda2d098dc386b3210a29a2af8
swank <swankyyy1@gmail.com>
2023-12-28 12:18:17 +0300

    work patch on all profile fields exclude photo

--- frontend/src/components/pages/profile_page.vue
+++ frontend/src/components/pages/profile_page.vue
@@ -20,23 +20,22 @@
                 </div>
                 <div class="info_row">Фамилия:
                     <div class="inner"><input class="form-control" type="text" placeholder="Введите фамилию"
-                    v-model="second_name">
+                                              v-model="last_name">
                         <button type="button" class="btn btn-primary">Обновить</button>
                     </div>
                 </div>
                 <div class="info_row">email:
                     <div class="inner"><input class="form-control" type="text" placeholder="Почтовый ящик"
-                    v-model="email">
+                                              v-model="email">
                         <button type="button" class="btn btn-primary">Обновить</button>
                     </div>
                 </div>
                 <div class="info_row">Дата рождения:
-                    <div class="inner"><input class="form-control" type="text" placeholder="Дата рождения"
-                    v-model="date_birth">
-                        <button type="button" class="btn btn-primary">Обновить</button>
+                    <div class="inner"><input class="form-control" type="date" placeholder="Дата рождения"
+                                              v-model="date_birth">
+                        <button type="button" class="btn btn-primary" @click="updater">Обновить</button>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
@@ -44,34 +43,63 @@
 
 <script>
 import {mapGetters} from "vuex";
+import axios from "axios";
 
 export default {
     name: "profile_page",
-    data(){
+    data() {
         return {
-            username: 'Нагибатор2000',
+            username: '',
             first_name: '',
-            second_name: '',
+            last_name: '',
             email: '',
             date_birth: '',
+            token: localStorage.getItem('token'),
         }
     },
-    methods:{
-      ...mapGetters({
-          url: "get_backend_url"
-      })
+    methods: {
+        ...mapGetters({
+            url: "get_backend_url",
+        }),
+        async updater() {
+            try {
+                let data = {
+                    'first_name': this.first_name,
+                    'last_name': this.last_name,
+                    'email': this.email,
+                    'date_birth': this.date_birth
+                }
+                let response = await axios.patch(this.url() + 'auth/users/me/', data)
+                console.log(response)
+            } catch (e) {
+                console.log(e)
+            }
+        }
     },
-    // async created() {
-    //     try {
-    //         let response = await axios.get(this.url() + 'auth/users/me/')
-    //     } catch (e) {
-    //         console.log(e)
-    //     }
-    // }
+    async created() {
+        try {
+            axios.defaults.headers.common['Authorization'] = this.token
+            let response = await axios.get(this.url() + 'auth/users/me/',)
+            console.log(response)
+            this.username = response.data.username
+            this.first_name = response.data.first_name
+            this.last_name = response.data.last_name
+            this.email = response.data.email
+            this.date_birth = response.data.date_birth
+        } catch (e) {
+            console.log(e)
+            console.log(this.token)
+        }
+    }
 }
 </script>
 
 <style scoped>
+p {
+    margin-top: 20px;
+    font-size: 12px;
+}
+
 .inner {
     display: flex;
 }
