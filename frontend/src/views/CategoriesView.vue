9dcc9e580d854d8539f2f7afdbac39ac1c527ef3
swank <swankyyy1@gmail.com>
2024-01-03 13:01:52 +0300

    test

---
+++ frontend/src/views/CategoriesView.vue
@@ -0,0 +1,25 @@
+<template>
+    <Header/>
+    <SelectedCategories :slug="slug"/>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+import SelectedCategories from "@/components/SelectedCategories.vue";
+
+export default {
+    name: "CategoriesView",
+    components: {SelectedCategories, Header},
+    props: {
+        slug: {
+            required: true,
+            type: String
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
