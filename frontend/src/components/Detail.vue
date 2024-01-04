0d602f6f6ccbb679dcd531518eb3376d8a8cac51
swank <swankyyy1@gmail.com>
2024-01-04 16:35:13 +0300

    att button for basket.add(item) hmm, it works:)

--- frontend/src/components/Detail.vue
+++ frontend/src/components/Detail.vue
@@ -9,7 +9,7 @@
                     <section>
                         <div class="container">
                             <div class="row mb-2">
-                                <h2>{{item.title}}</h2>
+                                <h2>{{ item.title }}</h2>
 
                             </div>
                             <div class="row">
@@ -18,16 +18,20 @@
 
                                 </div>
                                 <div class="col mt-3 ms-3 pt-2">
-                                    <p><b>Название: </b>{{item.title}}</p>
-                                    <p><b>Жанры: </b><div v-for="i in item.categories" class="genres">{{i}}, </div></p>
-                                    <p><b>Дата выхода: </b>{{item.year}}</p>
-                                    <p><b>Производство: </b>ASADASD</p>
-                                    <p><b>Язык озвучки: </b>ASDASDASDASDSADASD</p>
-                                    <p><b>Платформа: </b>ASDASDASDASDSADASD</p>
-                                    <p><b>Ограничения: </b>{{item.age_limit}}+</p>
+                                    <p><b>Название: </b>{{ item.title }}</p>
+                                    <p><b>Жанры: </b>
+                                    <div v-for="i in item.categories" class="genres">{{ i }},</div>
+                                    </p>
+                                    <p><b>Дата выхода: </b>{{ item.year }}</p>
+
+                                    <p><b>Ограничения: </b>{{ item.age_limit }}+</p>
                                     <div>
-                                        <h3 class="price">{{item.price}}р</h3>
+                                        <h3 class="price">{{ item.price }}р</h3>
                                     </div>
+                                    <button type="button" class="btn btn-warning p-2"
+                                            @click="buy"
+                                    v-bind:disabled="inbasket"
+                                    >{{text}}</button>
                                 </div>
                             </div>
                             <div class="row ps-2 pe-2 mt-2 mb-4">
@@ -60,21 +64,45 @@ export default {
         }
     },
     data() {
-      return {
-          item: {}
-      }
+        return {
+            item: {},
+            product_id: null,
+            user_id: null,
+            inbasket: false,
+            text: 'Добавить в корзину'
+        }
     },
 
     computed: {
         ...mapGetters({
-            url: "get_backend_url"
+            url: "get_backend_url",
+
+
         })
+    },
+    methods: {
+        async buy() {
+            let response = await axios.get(this.url + 'auth/users/me/',)
+            this.user_id = response.data.id
+            let data = {
+                'product_id': this.product_id,
+                'product_quantity': 1
+            }
+            response = await axios.post(this.url + `api/basket/basket/${this.user_id}`,
+                data)
+            if (response.status === 200) {
+                this.inbasket = true
+                this.text = 'В корзине'
+            }
+        }
     },
 
     async created() {
         try {
             let response = await axios.get(`http://127.0.0.1:8000/api/products/${this.slug}`)
             this.item = response.data
+            this.product_id = response.data.id
+
 
         } catch (e) {
 
@@ -87,6 +115,7 @@ export default {
 .poster_img {
     height: 300px;
 }
+
 .genres {
     display: inline;
 
